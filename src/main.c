38486b94db27c57265b2c3234020e6b1f87e9d3e
emfEpsilon <emf_epsilon@hotmail.com>
2024-03-26 23:20:37 -0300

    Simple greeting working

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include "greetings.h"
+#include <stdio.h>
+
+/**
+ * @brief Greet the user
+ * 
+ */
+int main(int argc, char* argv[])
+{
+    printf("Hello from main file!\n");
+    greetings();
+    return 0;
+}
